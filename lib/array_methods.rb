63c46f570b0bc5eb2a694e2840a57a9494f41002
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-28 02:41:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,10 @@ def find_element_index(array, value_to_find)
   while count < array.length do 
     if array[count] == value_to_find
       return count
-    
+    end
+    count += 1 
+  end
+  nil
 end
 
 def find_max_value(array)
