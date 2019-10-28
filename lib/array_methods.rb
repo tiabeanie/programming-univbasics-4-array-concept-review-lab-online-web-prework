cf5613dfcc727643ade8337750bb37c876163c9f
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-28 02:44:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,7 +12,8 @@ end
 def find_max_value(array)
   x = array[0]
   array.length.times { |index|
-    if array[index] >
+    if array[index] > x 
+      x = array[index]
 end
 
 def find_min_value(array)
