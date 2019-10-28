bc2084d078cb0c33d4cc33bfefc2532567e9d971
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-28 02:47:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,5 +20,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  
+  x = array[0]
+  array.length.times { |index|
+    if array[index] < x 
+      x = array[index]
+    end
+  }
+  x
+end
 end
